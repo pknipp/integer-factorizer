1d0961c32a710e05fd9aed9d1e078a153d980928
Peter Knipp <peteraknipp@gmail.com>
2021-11-16 14:19:30 -0500

    Change from slice of structs to slice of arrays.

--- main.go
+++ main.go
@@ -1,37 +1,29 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	// "io"
-	"encoding/json"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
+	// "encoding/json"
+	// "log"
+	// "net/http"
+	// "os"
+	// "strconv"
 	// "reflect"
 	// "strings"
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+	// "github.com/gin-gonic/gin"
+	// _ "github.com/heroku/x/hmetrics/onload"
 )
 
-func addFactor(j string, factors map[string]int) {
-	if _, ok := factors[j]; ok {
-		factors[j]++
-	} else {
-		factors[j] = 1
-	}
-}
+// type Factor struct {
+	// Prime   int
+	// Power   int
+// }
 
-type Factor struct {
-	Prime   int
-	Power   int
-}
-
-func factorize(number int) (bool, []Factor) {
+func factorize(number int) (bool, [][2]int) {
 	j := 1
-	factors := []Factor{}
+	factors := [][2]int{}
 	isPrime := true
-	var factor Factor
+	var factor [2]int
 	var facFound bool
 	// One only needs to search up until the square root of number.
 	for j * j < number {
@@ -45,13 +37,13 @@ func factorize(number int) (bool, []Factor) {
 		for {
 			if number % j == 0 {
 				if !facFound {
-					factor = Factor{Prime: j, Power: 1,}
+					factor[0], factor[1] = j, 1
 					facFound = true
 					if number > 2 {
 						isPrime = false
 					}
 				} else {
-					factor.Power++
+					factor[1]++
 				}
 				number /= j
 			} else {
@@ -65,25 +57,25 @@ func factorize(number int) (bool, []Factor) {
 	}
 	// The last factor is needed if the largest factor occurs by itself.
 	if !facFound && number != 1 {
-		factors = append(factors, Factor{Prime: number, Power: 1})
+		factors = append(factors, [2]int{number, 1})
 	}
 	return isPrime, factors
 }
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+	// port := os.Getenv("PORT")
+	// if port == "" {
+		// log.Fatal("$PORT must be set")
+	// }
 	// I opted not to use this version of router, for technical reasons.
 	// router := gin.New()
-	router := gin.Default()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	// router := gin.Default()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
+	// router.GET("/", func(c *gin.Context) {
+		// c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// })
 	// router.GET("/:number", func(c *gin.Context) {
 		// numberStr := c.Param("number")
 		// Eventually, I'll need to error-handle the following.
@@ -103,20 +95,20 @@ func main() {
 				// "isPrime": isPrime,
 		// })
 	// })
-	router.GET("/json/:number", func(c *gin.Context) {
-		numberStr := c.Param("number")
-		number, _ := strconv.Atoi(numberStr)
-		isPrime, result := factorize(number)
-		resultStr := "{\"number\": " + numberStr + ", \"isPrime\": " + strconv.FormatBool(isPrime)
-		if !isPrime {
-			factorStr, _ := json.Marshal(result)
-			resultStr += ", \"factors\": " + string(factorStr)
-		}
-		c.String(http.StatusOK, resultStr + "}")
-	})
-	router.Run(":" + port)
+	// router.GET("/json/:number", func(c *gin.Context) {
+		// numberStr := c.Param("number")
+		// number, _ := strconv.Atoi(numberStr)
+		// isPrime, result := factorize(number)
+		// resultStr := "{\"number\": " + numberStr + ", \"isPrime\": " + strconv.FormatBool(isPrime)
+		// if !isPrime {
+			// factorStr, _ := json.Marshal(result)
+			// resultStr += ", \"factors\": " + string(factorStr)
+		// }
+		// c.String(http.StatusOK, resultStr + "}")
+	// })
+	// router.Run(":" + port)
 	// Use the following when testing the app in a non-server configuration.
-	// number := 1234567890123456781
-	// bool, factoredString := factorize(number)
-	// fmt.Println(number, bool, factoredString)
+	number := 1234567890123456789
+	bool, factoredString := factorize(number)
+	fmt.Println(number, bool, factoredString)
 }
