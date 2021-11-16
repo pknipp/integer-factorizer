579da0b0cd8de3766fbb5a6ccbce90bba4307774
Peter Knipp <peteraknipp@gmail.com>
2021-11-15 20:29:09 -0500

    Convert main.go from CLI to web.

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	// "io"
-	// 	"log"
-	// "net/http"
-	// "os"
+		"log"
+	"net/http"
+	"os"
 	"strconv"
 	// "strings"
-	// "github.com/gin-gonic/gin"
-	// _ "github.com/heroku/x/hmetrics/onload"
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func addFactor(j string, factors map[string]int) {
@@ -40,38 +40,49 @@ func factorize(number int) map[string]int {
 }
 
 func main() {
-	// port := os.Getenv("PORT")
-	// if port == "" {
-		// log.Fatal("$PORT must be set")
-	// }
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	// I opted not to use this version of router, for technical reasons.
 	// router := gin.New()
-	// router := gin.Default()
-	// router.Use(gin.Logger())
-	// router.LoadHTMLGlob("templates/*.tmpl.html")
-	// router.Static("/static", "static")
-	// router.GET("/", func(c *gin.Context) {
-		// c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	// })
+	router := gin.Default()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
 	// expressionText := "your expression"
 	// resultText := "numerical value"
-	// router.GET("/:expression", func(c *gin.Context) {
-		// expression := doRegExp(c.Param("expression"))
-		// c.HTML(http.StatusOK, "result.tmpl.html", gin.H{
+	router.GET("/:number", func(c *gin.Context) {
+		numberString := c.Param("number")
+		// Eventually, I'll need to error-handle the following.
+		number, _ := strconv.Atoi(numberString)
+		result := factorize(number)
+		resultString := ""
+		for prime, power := range result {
+			resultString += prime
+			if power > 1 {
+				resultString += "<sup>" + strconv.Itoa(power) + "</sup>"
+			}
+		}
+		c.HTML(http.StatusOK, "result.tmpl.html", gin.H{
 				// "expressionText": expressionText,
 				// "expressionValue": expression,
 				// "resultText": resultText,
-				// "resultValue": handler(expression),
-		// })
-	// })
+				"number": number,
+				"resultString": resultString,
+		})
+	})
 	// router.GET("/json/:expression", func(c *gin.Context) {
 		// expression := doRegExp(c.Param("expression"))
 		// resultString := "{\"" + expressionText + "\": " + expression + ", \"" + resultText + "\": " + handler(expression) + "}"
 		// c.String(http.StatusOK, resultString)
 	// })
-	// router.Run(":" + port)
+	router.Run(":" + port)
 	// Use the following when testing the app in a non-server configuration.
-	number := 1234567890123456789
-	factoredString := factorize(number)
-	fmt.Println(number, factoredString)
+	// number := 1234567890123456789
+	// factoredString := factorize(number)
+	// fmt.Println(number, factoredString)
 }
