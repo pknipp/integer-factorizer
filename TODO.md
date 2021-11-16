f350f2295d4b587285b9a566d536702c90c59e2f
Peter Knipp <peteraknipp@gmail.com>
2021-11-16 18:03:41 -0500

    Minor changes.

--- TODO.md
+++ TODO.md
@@ -2,4 +2,3 @@ figure out how to import "math" into handler, which enable DRY-ing of some parts
 figure out how to separate functions into separate files
 use all caps for some constants, such as ...
     one, zero, pole
-DRY up unary by making a helper function that checks that arg is nonzero.
--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	// "fmt"
-	// "io"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	// "reflect"
-	// "strings"
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
@@ -30,7 +28,7 @@ func factorize(numberStr string) (bool, [][2]int, string) {
 	var facFound bool
 	// One only needs to search up until the square root of number.
 	for j * j < number {
-		// After 3, primes skip by at least two.
+		// After 3, only odd numbers can be prime.
 		if j < 3 {
 			j++
 		} else {
@@ -81,12 +79,10 @@ func main() {
 	})
 	router.GET("/:number", func(c *gin.Context) {
 		numberStr := c.Param("number")
-		// Eventually, I'll need to error-handle the following.
-		// number, _ := strconv.Atoi(numberStr)
 		isPrime, factors, message := factorize(numberStr)
 		c.HTML(http.StatusOK, "result.tmpl.html", gin.H{
 				"number": numberStr,
-				"isPrime": isPrime, //strconv.FormatBool(isPrime),
+				"isPrime": isPrime,
 				"factors": factors,
 				"message": message,
 		})
@@ -95,19 +91,21 @@ func main() {
 		numberStr := c.Param("number")
 		// number, _ := strconv.Atoi(numberStr)
 		isPrime, result, message := factorize(numberStr)
-		resultStr := "{\"number\": " + numberStr + ", \"isPrime\": " + strconv.FormatBool(isPrime)
+		resultStr := "{\"number\": " + numberStr
 		if len(message) > 0 {
 			resultStr += ", \"message\": " + message
-		}
-		if !isPrime {
-			factorStr, _ := json.Marshal(result)
-			resultStr += ", \"factors\": " + string(factorStr)
+		} else {
+			resultStr += ", \"isPrime\": " + strconv.FormatBool(isPrime)
+			if !isPrime {
+				factorStr, _ := json.Marshal(result)
+				resultStr += ", \"factors\": " + string(factorStr)
+			}
 		}
 		c.String(http.StatusOK, resultStr + "}")
 	})
 	router.Run(":" + port)
 	// Use the following when testing the app in a non-server configuration.
 	// number := 1234567890123456789
-	// bool, factors := factorize(number)
-	// fmt.Println(number, bool, factors)
+	// bool, factors, message := factorize(number)
+	// fmt.Println(number, bool, factors, message)
 }
