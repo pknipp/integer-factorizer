b1b7132b6697fe21a7b18cca982773d31ce64114
Peter Knipp <peteraknipp@gmail.com>
2021-11-16 10:07:00 -0500

    Convert main.go from web to CLI

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 	// "io"
-	"encoding/json"
-		"log"
-	"net/http"
-	"os"
+	// "encoding/json"
+	// "log"
+	// "net/http"
+	// "os"
 	"strconv"
 	// "reflect"
 	// "strings"
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+	// "github.com/gin-gonic/gin"
+	// _ "github.com/heroku/x/hmetrics/onload"
 )
 
 func addFactor(j string, factors map[string]int) {
@@ -24,6 +24,10 @@ func addFactor(j string, factors map[string]int) {
 
 func factorize(number int) (bool, map[string]int) {
 	j := 2
+	// type factor struct {
+		// Prime   int
+		// Power   int
+	// }
 	var factors = make(map[string]int)
 	isPrime := true
 	for j * j <= number {
@@ -44,57 +48,52 @@ func factorize(number int) (bool, map[string]int) {
 }
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+	// port := os.Getenv("PORT")
+	// if port == "" {
+		// log.Fatal("$PORT must be set")
+	// }
 	// I opted not to use this version of router, for technical reasons.
 	// router := gin.New()
-	router := gin.Default()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-	// expressionText := "your expression"
-	// resultText := "numerical value"
-	router.GET("/:number", func(c *gin.Context) {
-		numberStr := c.Param("number")
+	// router := gin.Default()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
+	// router.GET("/", func(c *gin.Context) {
+		// c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// })
+	// router.GET("/:number", func(c *gin.Context) {
+		// numberStr := c.Param("number")
 		// Eventually, I'll need to error-handle the following.
-		number, _ := strconv.Atoi(numberStr)
-		isPrime, result := factorize(number)
-		resultStr := ""
-		for prime, power := range result {
-			resultStr += `&nbsp;` + prime
-			if power > 1 {
-				resultStr += `<SUP>` + strconv.Itoa(power) + `</SUP>`
-			}
-		}
-		fmt.Println(resultStr)
-		c.HTML(http.StatusOK, "result.tmpl.html", gin.H{
-				// "expressionText": expressionText,
-				// "expressionValue": expression,
-				// "resultText": resultText,
-				"numberString": numberStr,
-				"resultString": resultStr,
-				"isPrime": isPrime,
-		})
-	})
-	router.GET("/json/:number", func(c *gin.Context) {
-		numberStr := c.Param("number")
-		number, _ := strconv.Atoi(numberStr)
-		isPrime, result := factorize(number)
-		resultStr := "{\"number\": " + numberStr + ", \"isPrime\": " + strconv.FormatBool(isPrime)
-		if !isPrime {
-			factorStr, _ := json.Marshal(result)
-			resultStr += ", \"factors\": " + string(factorStr)
-		}
-		c.String(http.StatusOK, resultStr + "}")
-	})
-	router.Run(":" + port)
+		// number, _ := strconv.Atoi(numberStr)
+		// isPrime, result := factorize(number)
+		// resultStr := ""
+		// for prime, power := range result {
+			// resultStr += `&nbsp;` + prime
+			// if power > 1 {
+				// resultStr += `<SUP>` + strconv.Itoa(power) + `</SUP>`
+			// }
+		// }
+		// fmt.Println(resultStr)
+		// c.HTML(http.StatusOK, "result.tmpl.html", gin.H{
+				// "numberString": numberStr,
+				// "resultString": resultStr,
+				// "isPrime": isPrime,
+		// })
+	// })
+	// router.GET("/json/:number", func(c *gin.Context) {
+		// numberStr := c.Param("number")
+		// number, _ := strconv.Atoi(numberStr)
+		// isPrime, result := factorize(number)
+		// resultStr := "{\"number\": " + numberStr + ", \"isPrime\": " + strconv.FormatBool(isPrime)
+		// if !isPrime {
+			// factorStr, _ := json.Marshal(result)
+			// resultStr += ", \"factors\": " + string(factorStr)
+		// }
+		// c.String(http.StatusOK, resultStr + "}")
+	// })
+	// router.Run(":" + port)
 	// Use the following when testing the app in a non-server configuration.
-	// number := 1234567890123456789
-	// factoredString := factorize(number)
-	// fmt.Println(number, factoredString)
+	number := 1234567890123456789
+	bool, factoredString := factorize(number)
+	fmt.Println(number, bool, factoredString)
 }
