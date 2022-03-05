fc8610acb0d2644d433dd986d241f6f99c00dc07
Peter Knipp <peteraknipp@gmail.com>
2022-03-05 17:59:34 -0500

    Make template-fields naming consistent.

--- main.go
+++ main.go
@@ -50,8 +50,8 @@ func main() {
 				if strings.Count(inputStr, ".") == 1 {
 					result, message := decimal(inputStr)
 					c.HTML(http.StatusOK, "result.tmpl.html", gin.H{
-						"inputNonrepeating": result.nonrepeating,
-						"inputRepeating": result.repeating,
+						"nonrepeating": result.nonrepeating,
+						"repeating": result.repeating,
 						"message": message,
 						"type": "decimal",
 						"title": "Decimal to fraction conversion",
