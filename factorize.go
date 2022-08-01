2ffc2f8bed34725130f4fe66558e47a8cf7eabd7
Peter Knipp <peteraknipp@gmail.com>
2022-08-01 07:55:11 -0400

    Incorporate big fully in factorize.go

--- factorize.go
+++ factorize.go
@@ -2,31 +2,34 @@ package main
 
 import (
 	"sort"
+	"math/big"
 )
 
-func factorize(number int) (bool, [][2]int) {
+func factorize(number *big.Int) (bool, [][2]*big.Int) {
 	j := 1
+	jBig := big.NewInt(int64(j))
 	// Use a map for storing factors, to facilitate the algo.
 	// key = prime, value = exponent
-	factors := map[int]int{}
+	factors := map[int]*big.Int{}
 	// One only needs to search up until the square root of number.
-	for j * j < number {
+	j2Big := big.NewInt(0).Mul(jBig, jBig)
+	for j2Big.Cmp(number) == -1 {
 		// After 3, only odd numbers can be prime (so step-size = 2, then)
-		if j < 3 {
-			j++
+		if jBig.Cmp(big.NewInt(3)) == -1 {
+			jBig.Add(jBig, big.NewInt(1))
 		} else {
-			j += 2
+			jBig.Add(jBig, big.NewInt(2))
 		}
 		for {
 			// Continue dividing out (and tabulating) j until j is no longer a factor of number.
-			if number % j == 0 {
+			if big.NewInt(0).Mod(number, jBig).Cmp(big.NewInt(0)) == 0 {
 				_, facFound := factors[j]
 				if facFound {
-					factors[j]++
+					factors[j].Add(factors[j], big.NewInt(1))
 				} else {
-					factors[j] = 1
+					factors[j] = big.NewInt(1)
 				}
-				number /= j
+				number.Div(number, jBig)
 			} else {
 				// Go to next possible factor.
 				break
@@ -34,15 +37,15 @@ func factorize(number int) (bool, [][2]int) {
 		}
 	}
 	// The last factor is needed if the largest factor occurs by itself.
-	if number != 1 {
-		factors[number] = 1
+	if number.Cmp(big.NewInt(1)) != 0 {
+		factors[int(number.Int64())] = big.NewInt(1)
 	}
 	// Below is a necessary - but not sufficient - condition of primacy.
 	isPrime := len(factors) == 1
 	// The condition below is required to make it "sufficient".
 	if isPrime {
 		for _, exponent := range factors {
-			if exponent > 1 {
+			if exponent.Cmp(big.NewInt(1)) == 1 {
 				isPrime = false
 			}
 		}
